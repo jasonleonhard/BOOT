996af8d668bcadfe7fd645cd7cdda636e558346f
un5t0ppab13 <devbright@gmail.com>
2015-03-16 11:47:30 -0700

    about page without _jumbotron partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  get 'about/index'
+#  get 'about/index'
+  get 'about' => 'about#index'
+
   get 'home/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
